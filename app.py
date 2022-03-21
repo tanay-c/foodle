03dde42ba6fec25e0456855b3561278c6d093260
tanay <tanaychougule@gmail.com>
2022-03-20 20:08:47 -0500

    initial flask template

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, url_for, render_template, request
+# escape helps against injection attacks
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hello"
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    # show the user profile for that user
+    if request.method == 'POST':
+        return 'login'
+    else:
+        return 'show_login_page'
+
+@app.route('/user/<username>')
+def profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+if (__name__ == '__main__'):
+    app.run(debug=True)
